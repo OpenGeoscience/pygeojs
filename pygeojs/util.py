ae7c111483d62803e0a166bc0dc355fbba2c48c7
John Tourtellott <john.tourtellott@kitware.com>
2019-04-13 18:19:32 -0400

    Fix bug setting center & zoom before geojs map initialized
    Also reorder pygeojs/util.py (no functional change)

--- pygeojs/util.py
+++ pygeojs/util.py
@@ -7,24 +7,24 @@ import json
 import os
 import urllib
 
-def _lookup_mime(path):
-    """Determines mime type from extension"""
-    base,ext = os.path.splitext(path)
-    if not ext:
-        return None
-    ext = ext.lower()
-    mime_dict = {
-        '.bmp': 'image/bmp',
-        '.gif': 'image/gif',
-        '.ico': 'image/vnd.microsoft.icon',
-        '.jpeg': 'image/jpeg',
-        '.jpg': 'image/jpeg',
-        '.png': 'image/png',
-        '.svg': 'image/svg+xml',
-        '.webp': 'image/webp'
-    }
-    # (Left off image/tiff because no browser support)
-    return mime_dict.get(ext)
+
+def get_tiles_url(girder_url, item_id, mapnik_style=None):
+    """Constructs tiles url for girder large-image display
+
+    """
+    base_url = '{}/api/v1/item/{}/tiles/zxy/{{z}}/{{x}}/{{y}}'.format(
+        girder_url, item_id)
+    mapnik_string = ''
+    if mapnik_style:
+        if isinstance(mapnik_style, str):
+            style_string = mapnik_style
+        else:
+            style_string = json.dumps(mapnik_style)
+        encoded_string = urllib.parse.quote_plus(style_string)
+        mapnik_string = '&style={}'.format(encoded_string)
+    tiles_url = '{}?encoding=PNG&projection=EPSG:3857{}'.format(
+        base_url, mapnik_string)
+    return tiles_url
 
 
 def img2base64(source, mime=None):
@@ -74,20 +74,22 @@ def img2base64(source, mime=None):
     encoded_data = prefix + encoded_bytes.decode('ascii')
     return encoded_data
 
-def get_tiles_url(girder_url, item_id, mapnik_style=None):
-    """Constructs tiles url for girder large-image display
 
-    """
-    base_url = '{}/api/v1/item/{}/tiles/zxy/{{z}}/{{x}}/{{y}}'.format(
-        girder_url, item_id)
-    mapnik_string = ''
-    if mapnik_style:
-        if isinstance(mapnik_style, str):
-            style_string = mapnik_style
-        else:
-            style_string = json.dumps(mapnik_style)
-        encoded_string = urllib.parse.quote_plus(style_string)
-        mapnik_string = '&style={}'.format(encoded_string)
-    tiles_url = '{}?encoding=PNG&projection=EPSG:3857{}'.format(
-        base_url, mapnik_string)
-    return tiles_url
+def _lookup_mime(path):
+    """Determines mime type from extension"""
+    base,ext = os.path.splitext(path)
+    if not ext:
+        return None
+    ext = ext.lower()
+    mime_dict = {
+        '.bmp': 'image/bmp',
+        '.gif': 'image/gif',
+        '.ico': 'image/vnd.microsoft.icon',
+        '.jpeg': 'image/jpeg',
+        '.jpg': 'image/jpeg',
+        '.png': 'image/png',
+        '.svg': 'image/svg+xml',
+        '.webp': 'image/webp'
+    }
+    # (Left off image/tiff because no browser support)
+    return mime_dict.get(ext)
